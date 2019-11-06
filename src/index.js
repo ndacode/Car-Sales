99411d3e5319b8a6649421dd636fa397d8c04e98
Nicole Adams <nicoleadams.boston@gmail.com>
2019-11-06 18:58:32 -0500

    Add store, reducer, Provider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+
+function reducer () {
+    return {
+      title: "Reducer"
+    }
+  }
+  
+
+const store = createStore(reducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>
+, rootElement);
